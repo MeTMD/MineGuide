4d886a06015c6035b4c450388466b7ef52994666
Harry Huang <harryhuang2652@qq.com>
2025-04-29 17:49:06 +0800

    feat: add llm related config field

--- src/Data.py
+++ src/Data.py
@@ -5,13 +5,15 @@ import configparser
 
 class MineGuideConfig:
     def __init__(self, file: TextIO):
-        """Loads config from a file handler that provides INI format content.
-        """
-        parser = configparser.ConfigParser()
+        """Loads config from a file handler that provides INI format content."""
+        parser = configparser.ConfigParser(interpolation=None)
         parser.read_string(file.read())
         self._host = parser["Connection"]["host"]
         self._port = parser["Connection"]["port"]
         self._username = parser["Connection"]["username"]
+        self._llm_module = parser["LLM"]["module"]
+        self._llm_model_name = parser["LLM"]["model_name"]
+        self._llm_client = parser["LLMClient"]
 
     @property
     def host(self):
@@ -24,3 +26,15 @@ class MineGuideConfig:
     @property
     def username(self):
         return self._username
+
+    @property
+    def llm_module(self):
+        return self._llm_module
+
+    @property
+    def llm_model_name(self):
+        return self._llm_model_name
+
+    @property
+    def llm_client(self):
+        return self._llm_client
