9edfa7999ce535d5c9be40756b394e9487c12cb8
Harry Huang <harryhuang2652@qq.com>
2025-04-30 19:44:58 +0800

    feat: robust loading

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,10 +1,12 @@
 import re
+import time
 
 from .api.BotAPI import BotAPI
 from .Data import MineGuideConfig
 from .chat.Agent import Agent
 
-FC = re.compile(r"FCFC::(\w+?)((::.+?)*?)::CFCF")
+ENV_FILE = "mg.config.env"
+FC_PATTERN = re.compile(r"FCFC::(\w+?)((::.+?)*?)::CFCF")
 
 
 def handle_msg(agent: Agent, bot: BotAPI, username: str, message: str):
@@ -21,7 +23,7 @@ def handle_msg(agent: Agent, bot: BotAPI, username: str, message: str):
     else:
         print(f"{username} said {message}")
         for sentence in agent.chat(username, user_pos, self_pos, message):
-            if fc_match := FC.match(sentence):
+            if fc_match := FC_PATTERN.match(sentence):
                 try:
                     fc_name = fc_match.group(1)
                     fc_params = tuple(filter(lambda x: x, fc_match.group(2).split("::")))
@@ -34,22 +36,39 @@ def handle_msg(agent: Agent, bot: BotAPI, username: str, message: str):
                 bot.do_chat(sentence)
 
 def main():
-    print("Loading config")
-    config = MineGuideConfig(open("mg.config.env"))
-
-    print("Tuning LLM")
-    agent = Agent(config)
-
-    print("Starting bot")
-    bot = BotAPI(config)
-    bot.on_login = lambda: print("Bot login succeeded")
-    bot.on_receive_message = lambda x, y: handle_msg(agent, bot, x, y)
-    bot.connect()
-
-    while True:
-        import time
-
-        time.sleep(1)
+    print("Welcome to MineGuide backend!")
+    try:
+        print("Loading config")
+        config = MineGuideConfig(open("mg.config.env"))
+    except FileNotFoundError:
+        print("Failed to load env config")
+        print(f"Please ensure the file '{ENV_FILE}' exists")
+    except KeyError as arg:
+        print("Failed to load env config")
+        print(f"Missing config section or key '{arg}'")
+    except Exception as arg:
+        print("Failed to load env config")
+        print(f"Cause '{arg}'")
+    else:
+        try:
+            print("Tuning LLM")
+            agent = Agent(config)
+        except Exception:
+            print("Failed to access LLM API")
+            print("Please ensure your LLM API config")
+        else:
+            try:
+                print("Starting bot")
+                bot = BotAPI(config)
+                bot.on_login = lambda: print("Bot login succeeded")
+                bot.on_receive_message = lambda x, y: handle_msg(agent, bot, x, y)
+                bot.connect()
+            except Exception:
+                print("Failed to connect to the server")
+            else:
+                print("Press Ctrl+C to stop running")
+                while True:
+                    time.sleep(1)
 
 
 if __name__ == "__main__":
